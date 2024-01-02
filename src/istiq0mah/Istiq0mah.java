42b2a708e37f773706b3ff5c80b73ee387fcfe22
Ammar Dzakwan <Ammar Dzakwan@ZAPHIREZ>
2024-01-02 23:29:42 +0700

    Test

---
+++ src/istiq0mah/Istiq0mah.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package istiq0mah;
+
+/**
+ *
+ * @author Ammar Dzakwan
+ */
+public class Istiq0mah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Frame1Salam f1 = new Frame1Salam();
+        f1.setLocationRelativeTo(null);
+        f1.setVisible(true);
+    }
+    
+}
